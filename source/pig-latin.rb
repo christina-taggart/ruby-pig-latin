1b6ee1f74e8f88b9ec20d166285849d7a277464b
Apprentice <apprentice@dbc25.local>
2014-01-07 15:59:32 -0800

    Release 1 Pig latin

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -13,3 +13,24 @@ def pig_latin
 end
 
 pig_latin
+
+# Your code here
+def pig_latin
+  puts "Please enter a sentence"
+  sentence = gets.chomp #tree is tall
+  words = sentence.split(" ") #["tree", "is", "tall"]
+  pig_latin_sentence = ""
+  words.each do |word|
+    letters = word.split("") #[t,r,e,e]
+    if letters[0] =~ /[aeiou]/
+      pig_latin_word = word
+    else
+      /(?<first_consonants>[^aeiou]*)(?<rest_of_word>.*)/ =~ word
+      pig_latin_word = rest_of_word + first_consonants  + "ay"
+    end
+    pig_latin_sentence << pig_latin_word + " "
+  end
+  puts pig_latin_sentence
+end
+
+pig_latin
