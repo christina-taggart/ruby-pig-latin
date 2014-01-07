41c4d229c8cee93faf529f63c57f437e872f6994
Apprentice <apprentice@dbc28.local>
2014-01-07 15:51:28 -0800

    Release 1: Added sentence functionality

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -1,15 +1,22 @@
-# Your code here
-
-#if a e i o u, do nothing
-#else, do stuff
-
 def pig_latin (word)
   if /[^aeiou]/.match(word.split('').first)
-     return word.sub(/([^aeiou]*)(.*)/){"#{$2}-#{$1}ay"}
+     return word.sub(/([^aeiouy]*)(.*)(\W*)/){"#{$2}-#{$1.downcase}ay"}
   end
   word
 end
 
-p pig_latin("string")
-# == "ingstray"
+puts "Enter sentence to convert to pig latin:"
+words = gets.chomp.split(' ')
+pig_latin_sentence = ""
+num_of_changed = 0
+words.each do |word|
+  pig_latin_sentence += " " + pig_latin(word)
+  num_of_changed += 1 if pig_latin(word) != word
+end
+p pig_latin_sentence.strip
+p "Number of words: #{num_of_changed}"
+
+
+## TESTS ##
+p pig_latin("string") == "ing-stray"
 p pig_latin("apples") == "apples"
