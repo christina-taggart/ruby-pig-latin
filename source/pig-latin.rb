05f296bd82745ebe9d35d8bb848097140a57ddee
Apprentice <apprentice@dbc08.local>
2014-01-07 10:40:38 -0800

    Iteration 1: Can convert single word to pig latin

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -1,1 +1,14 @@
-# Your code here
+def pig_latin
+  puts "Enter what you want converted to pig latin."
+  unconverted = gets.chomp
+
+  if unconverted.match(/\A[^aeiou]/)
+    consonants = unconverted.slice!(0, unconverted.index(/[aeiou]/))
+    puts unconverted + consonants + "ay"
+  else
+    puts unconverted
+  end
+
+end
+
+pig_latin
