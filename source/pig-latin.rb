d970f2175fd1d56213d4c00db0e7ee5ef18b48f9
Apprentice <apprentice@dbc03.local>
2014-01-07 10:54:51 -0800

    Add pig_latin for sentences (iteration two)

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -8,12 +8,24 @@
 #     add first character to the end
 #     add 'ay'
 
-VOWELS = ['a','e','i','o','u']
+VOWELS = ['a','e','i','o','u','A','E','I','O','U']
 
-def pig_latin(word)
+def pig_latin_sentence(sentence)
+  words = sentence.split(" ")
+  converted_words = []
+  words.map do |word|
+    pig_latin_word(word, converted_words)
+  end
+  puts "This many words were changed: #{converted_words.length} words."
+  words.join(" ")
+end
+
+
+def pig_latin_word(word, converted_words)
   if word.length >0 && !VOWELS.include?(word[0])
     first_char = word.slice!(0)
     word = word.concat(first_char).concat('ay')
+    converted_words << word
   end
 
   word
@@ -22,6 +34,8 @@ end
 
 # driver
 
-p pig_latin("farmer") == "armerfay"
-p pig_latin("egg") == "egg"
-p pig_latin("hour") == "ourhay"
+p pig_latin_sentence("farmer") == "armerfay"
+p pig_latin_sentence("egg") == "egg"
+p pig_latin_sentence("hour") == "ourhay"
+p pig_latin_sentence("This is a sentence") == "hisTay is a entencesay"
+p pig_latin_sentence("Everything is okay.") == "Everything is okay."
