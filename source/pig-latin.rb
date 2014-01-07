0831c71625150ebcfa569e524060dfeda48a5e85
Apprentice <apprentice@dbc03.local>
2014-01-07 10:41:49 -0800

    Add pig latin single word

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -1,1 +1,27 @@
 # Your code here
+
+# check the first character
+#   if its a vowel
+#     do nothing
+#   if its a consonant
+#     remove first character
+#     add first character to the end
+#     add 'ay'
+
+VOWELS = ['a','e','i','o','u']
+
+def pig_latin(word)
+  if word.length >0 && !VOWELS.include?(word[0])
+    first_char = word.slice!(0)
+    word = word.concat(first_char).concat('ay')
+  end
+
+  word
+end
+
+
+# driver
+
+p pig_latin("farmer") == "armerfay"
+p pig_latin("egg") == "egg"
+p pig_latin("hour") == "ourhay"
