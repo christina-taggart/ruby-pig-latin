19549e13395bc8810d8e6e31c8bef59196a0d981
Apprentice <apprentice@dbc29.local>
2014-01-07 15:27:04 -0800

    Release 0 Iteration 2 added convert_sentence_to_pig_latin method

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -1,17 +1,12 @@
-# GET a word from user input.
-# IF the word starts with a vowel, don't change it.
-# ELSE replace the word with its pig latin equivalent.
-#   GET all of the consonants before the first vowel in the word.
-#   SET the consonants at the end of the word and add the suffix "ay."
-# ENDIF
-# PRINT the pig-latin-ified word.
-
-
-def convert_word_to_pig_latin
-  word = gets.chomp
-  return puts word if word.match(/^[aeiouy]/i)
+def convert_word_to_pig_latin(word)
+  return "#{word} "  if word.match(/^[aeiouy]/i)
   consonants = word.match(/^[^aeiouy]+/i).to_s
-  puts "#{word.gsub!(consonants, "")}#{consonants}ay"
+  "#{word.gsub!(consonants, "")}#{consonants}ay "
 end
 
-convert_word_to_pig_latin
+def convert_sentence_to_pig_latin
+  sentence = gets.chomp
+  puts sentence.split(" ").map { |word| convert_word_to_pig_latin(word) }.join("")
+end
+
+convert_sentence_to_pig_latin
