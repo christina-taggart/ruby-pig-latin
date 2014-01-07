bf82532cb49a9cc379021f99c5c95fe6fbde81f5
Apprentice <apprentice@DBC-Infinity-3.local>
2014-01-07 10:57:45 -0800

    Added named captures to gsub

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -1,16 +1,16 @@
 # Your code here
 def pig_latin(english)
   english.downcase!
-  vowels = ["a", "e", "i", "o", "u", "y"]
-  return english if vowels.include?(english[0]) || english == ""
-  english.gsub! (/^([^aeiouy]+)(.*)/) { $2 + $1 + "ay"}
+  return english if english == "" || "aeiouy".include?(english[0])
+  english.gsub! (/^(?<consonants>[^aeiouy]+)(?<rest_of_word>.*)/) do
+    $~[:rest_of_word] + $~[:consonants] + "ay"
+  end
 end
 
 
 #-----drivers-----
 def tester(expected, actual)
-  actual = pig_latin(actual)
-  puts "#{expected == actual} | #{expected} | #{actual} |"
+  puts "#{expected == pig_latin(actual)} | #{expected} | #{pig_latin(actual)}"
 end
 
 tester("ymgay", "gym")
