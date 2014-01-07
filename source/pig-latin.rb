7389f10b7a7747ea871706b97103b96b8155c9e5
Apprentice <apprentice@dbc08.local>
2014-01-07 10:52:21 -0800

    Iteration 2: Converts sentences

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -1,14 +1,14 @@
 def pig_latin
   puts "Enter what you want converted to pig latin."
-  unconverted = gets.chomp
+  puts gets.chomp.split.map{|word| pig_latin_single(word.downcase)}.join(" ")
+end
 
-  if unconverted.match(/\A[^aeiou]/)
-    consonants = unconverted.slice!(0, unconverted.index(/[aeiou]/))
-    puts unconverted + consonants + "ay"
-  else
-    puts unconverted
+def pig_latin_single(user_word)
+  if user_word.match(/\A[^aeiou]/)
+    consonants = user_word.slice!(0, user_word.index(/[aeiou]/))
+    user_word += consonants + "ay"
   end
-
+  user_word
 end
 
 pig_latin
