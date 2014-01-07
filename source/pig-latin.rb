8df986abee227ed1709e0ff7b5f634f180efbfd5
Apprentice <apprentice@dbc28.local>
2014-01-07 11:00:21 -0800

    Release 0: created pig latin method

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -1,1 +1,15 @@
 # Your code here
+
+#if a e i o u, do nothing
+#else, do stuff
+
+def pig_latin (word)
+  if /[^aeiou]/.match(word.split('').first)
+     return word.sub(/([^aeiou]*)(.*)/){"#{$2}-#{$1}ay"}
+  end
+  word
+end
+
+p pig_latin("string")
+# == "ingstray"
+p pig_latin("apples") == "apples"
