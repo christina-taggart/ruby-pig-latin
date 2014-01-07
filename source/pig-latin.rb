17a45fb35b2359c02133e7fc7daf8aa56fa97970
Apprentice <apprentice@dbc29.local>
2014-01-07 10:49:18 -0800

    Release 0 added convert_word_to_pig_latin(word) method which converts a single english word to pig latin

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -1,1 +1,17 @@
-# Your code here
+# GET a word from user input.
+# IF the word starts with a vowel, don't change it.
+# ELSE replace the word with its pig latin equivalent.
+#   GET all of the consonants before the first vowel in the word.
+#   SET the consonants at the end of the word and add the suffix "ay."
+# ENDIF
+# PRINT the pig-latin-ified word.
+
+
+def convert_word_to_pig_latin(word)
+  #input = gets.chomp
+  return word if word.match(/^[aeiouy]/i)
+  consonants = word.match(/^[^aeiouy]+/i).to_s
+  return "#{word.gsub!(consonants, "")}#{consonants}ay"
+end
+
+p convert_word_to_pig_latin('throne')
