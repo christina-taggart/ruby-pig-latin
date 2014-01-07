314f1363539d6dea33d512a9d097bdc14cda8686
Apprentice <apprentice@dbc25.local>
2014-01-07 15:09:09 -0800

    Pig Latin Challenege Completed

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -1,1 +1,15 @@
 # Your code here
+def pig_latin
+  puts "Please enter a word"
+  word = gets.chomp #tree
+  letters = word.split("") #[t,r,e,e]
+  if letters[0] == /[aeiou]/
+    pig_latin_word = word
+  else
+    /(?<first_consonants>[^aeiou]*)(?<rest_of_word>.*)/ =~ word
+    pig_latin_word = rest_of_word + first_consonants  + "ay"
+  end
+  puts pig_latin_word
+end
+
+pig_latin
