fa37ff57a819d103ffcbb05fc0be17f551bb77b3
Apprentice <apprentice@DBC-Infinity-3.local>
2014-01-07 10:38:55 -0800

    Adds release 0 iteration 1 solution

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -1,1 +1,20 @@
 # Your code here
+def pig_latin(english)
+  english.downcase!
+  vowels = ["a", "e", "i", "o", "u", "y"]
+  return english if vowels.include?(english[0]) || english == ""
+  english.gsub! (/^([^aeiouy]+)(.*)/) { $2 + $1 + "ay"}
+end
+
+
+#-----drivers-----
+def tester(expected, actual)
+  actual = pig_latin(actual)
+  puts "#{expected == actual} | #{expected} | #{actual} |"
+end
+
+tester("ymgay", "gym")
+tester("aardvark", "aardvark")
+tester("ohnjay", "John")
+tester("oolbusschay", "schoolbus")
+tester("", "")
