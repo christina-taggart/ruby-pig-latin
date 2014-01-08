e5397341c33682db6d6cc7f2a255015e8a12e47a
John Olmsted <johnaolmsted@gmail.com>
2014-01-07 18:47:16 -0800

    Adds javascript version of pig_latin_sentence

--- source/pig-latin.js
+++ source/pig-latin.js
@@ -1,13 +1,22 @@
+var _ = require('lodash');
+
 var pig_latin = function(english) {
   english = english.toLowerCase();
   if (english == "" || english.match(/^[aeiou]/))
   {
-    return english
+    return english;
   } 
   else 
   {
-    return english.replace(/^([^aeiouy\d\W_]+)(.*)/, "$2$1ay")
+    return english.replace(/^([^aeiouy\d\W_]+)(.*)/, "$2$1ay");
   }
 }
 
-console.log(pig_latin("hello"))
+var pig_latin_sentence = function(english) {
+  english_array = english.split(/\W+/);
+  piglatin_array = _(english_array).map(function(word) { return pig_latin(word) });
+  return piglatin_array.join(" ");
+}
+
+console.log(pig_latin("hello"));
+console.log(pig_latin_sentence("hello there my aardvark"));
