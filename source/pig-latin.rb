5345185efb2cc84fc0abbf45ef9ab7bd7ce2d1dd
Marion <knight499@gmail.com>
2014-01-07 16:22:16 -0800

    Completed Challenge

--- source/pig-latin.rb
+++ source/pig-latin.rb
@@ -1,1 +1,27 @@
-# Your code here
+def to_pig_latin(word)
+  if word.match(/\A([aeiou])/)
+    p word
+  else
+    after_vowel = word.slice(/([aeiou])\w*/)
+    before_vowel = word.slice(/^\w([^aeiou]*)/)
+    p after_vowel + before_vowel + "ay"
+  end
+end
+
+def pig_latin
+  puts "Give me a word!"
+  word = gets.chomp.downcase
+  to_pig_latin(word)
+end
+
+def pig_sentence
+  puts "Give me a sentence!"
+  sentence = gets.chomp.downcase
+  complete = sentence.split.map {|x| to_pig_latin(x)
+  }.join(" ")
+  p complete
+end
+
+
+pig_latin
+pig_sentence
